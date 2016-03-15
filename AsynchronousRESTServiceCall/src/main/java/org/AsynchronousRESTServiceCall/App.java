ef7897c3c84337a471404993df80c8da1e68b91f
MafaldaLandeiro <1110590@isep.ipp.pt>
2016-03-15 19:42:44 +0000

    Project creation

---
+++ AsynchronousRESTServiceCall/src/main/java/org/AsynchronousRESTServiceCall/App.java
@@ -0,0 +1,13 @@
+package org.AsynchronousRESTServiceCall;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
